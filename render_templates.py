7e49b585531b4f6d02aa6b7fce56dc2ceee20069
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-08-04 11:40:17 +0300

    fixed some misprints and edited the style

--- render_templates.py
+++ render_templates.py
@@ -3,7 +3,10 @@ from staticjinja import make_site
 
 OUTPATH = "site"
 
+def index_context(template):
+    return {'page': 'index'}
 
 if __name__ == "__main__":
-    site = make_site(outpath=OUTPATH)
+    site = make_site(outpath=OUTPATH,
+                     contexts=[('index.html', index_context)])
     site.render()
